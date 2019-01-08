44c93bd7a1ce7f4b0a8b5a8e707469fe1d7f679e
sailorbob134280 <sailorbob3.0@gmail.com>
2019-01-08 13:17:32 -0800

    dbman update

--- db_man.py
+++ db_man.py
@@ -5,14 +5,14 @@ from sqlite3 import Error
 
 class DatabaseManager:
 
-    def __init__(self, db_filename)
+    def __init__(self, db_filename='wininv-data'):
+        self.db_filename = db_filename
+        self.db_path = os.getcwd() + db_filename
 
-    def get_version(db_filename='wininv-data'):
+    def get_version(self):
         conn = None
         try:
-            db_dir = os.getcwd()
-            db_path = db_dir + db_filename
-            conn = sqlite3.connect(db_path)
+            conn = sqlite3.connect(self.db_path)
             version = sqlite3.version
             return version
         except Error as e:
@@ -20,5 +20,53 @@ class DatabaseManager:
         finally:
             if conn is not None:
                 conn.close()
+    
+    def db_execute(self, command, placeholders=()):
+        conn = None
+        try:
+            conn = sqlite3.connect(self.db_path)
+            cursor = conn.cursor()
+            cursor.execute(command, placeholders)
+            conn.commit()
+        except Error as e:
+            print(e)
+            conn.rollback()
+            print('Transaction failed due to an error. Database has been rolled back.')
+        finally:
+            if conn is not None:
+                conn.close()
 
+    def db_fetch(self, command, placeholders=(), rows='one'):
+        conn = None
+        try:
+            conn = sqlite3.connect(self.db_path)
+            cursor = conn.cursor()
+            cursor.execute(command, placeholders)
+            if rows is 'one':
+                return cursor.fetchone()
+            elif rows is 'all':
+                return cursor.fetchall()
+            elif isinstance(rows, not int):
+                raise FetchError('Rows must be one, all, or an integer')
+            else:
+                return cursor.fetchmany(rows)
+        except FetchError as fe:
+            print(fe)
+        except Error as e:
+            print(e)
+        finally:
+            if conn is not None:
+                conn.close()
+        
 
+########
+#Errors#
+########
+class Error(Exception):
+    '''Base class for custom error handling during database operations'''
+    pass
+
+class FetchError(Error):
+    '''Error to be raised if rows argument is out of range'''
+    def __init__(self, message):
+        self.message = message
