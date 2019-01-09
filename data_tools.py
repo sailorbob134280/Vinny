43dc9e07bb79040d4b3be6e683ada50dcbd2a0eb
sailorbob134280 <sailorbob3.0@gmail.com>
2019-01-09 13:51:16 -0800

    data tools now support usage on any table

--- data_tools.py
+++ data_tools.py
@@ -1,24 +1,44 @@
 from db_man import DatabaseManager
 
-def cleanup_wine_input(input_list):
-    # first, assign the inputs to a dictionary (including none)
-    wine_input = {"wine_id":input_list[0],
-                  "winery":input_list[1],
-                  "region":input_list[2],
-                  "name":input_list[3],
-                  "varietal":input_list[4],
-                  "wtype":input_list[5],
-                  "vintage":input_list[6],
-                  "msrp":input_list[7],
-                  "value":input_list[8]}
-    # if the term is not used, it is discarded from the search
+def cleanup_dbinput(input_list, table):
+    # first, figure out which table the input is for 
+    # and assign the inputs to a dictionary (including none)
+    if table is 'userinventory':
+        input_dict = {"transaction_id":input_list[0],
+                      "wine_id":input_list[1],
+                      "user_id":input_list[2],
+                      "qty":input_list[3],
+                      "bottle_size":input_list[4],
+                      "location":input_list[5],
+                      "comments":input_list[6],
+                      "date_in":input_list[7],
+                      "date_out":input_list[8]}
+    elif table is 'winedata':
+        input_dict = {"wine_id":input_list[0],
+                      "winery":input_list[1],
+                      "region":input_list[2],
+                      "name":input_list[3],
+                      "varietal":input_list[4],
+                      "wtype":input_list[5],
+                      "vintage":input_list[6],
+                      "msrp":input_list[7],
+                      "value":input_list[8]}
+    elif table is 'userdata':
+        input_dict = {"user_id":input_list[0],
+                      "username":input_list[1],
+                      "password":input_list[2],
+                      "cellar_space":input_list[3]}
+    else:
+        raise Exception('''Table does not exist! Please contact the devs about this... You really shouldn't see this.''')
+
+    # if the term is not used, it is discarded from the dictionary
     terms = {}
-    for wkey in wine_input:
-        if wine_input[wkey] is not None:
-            terms[wkey] = wine_input[wkey]
+    for wkey in input_dict:
+        if input_dict[wkey] is not None:
+            terms[wkey] = input_dict[wkey]
     return terms
 
-def search_winedb(search_input):
+def search_db(search_input, table='userinventory'):
     # searches for a wine in the database that resembles the input
     # first, assign the inputs to a list to be cleaned up
     # this function takes a list of the following format:
@@ -28,19 +48,19 @@ def search_winedb(search_input):
     # it is critical that the list is complete, even if some
     # of the values are null
 
-    terms = cleanup_wine_input(search_input)
+    terms = cleanup_dbinput(search_input, table)
 
     # craft the sql query string
-    arg = 'SELECT * FROM winedata WHERE '
+    arg = 'SELECT * FROM ' + table + ' WHERE '
     for term in terms:
         arg += '{0} LIKE {1} AND '.format(term, ':' + term)
-    arg = arg.rstrip(' AND ') + ';'
+    arg = arg.rstrip(' AND ')
 
     # finally, call the search function from the db_man object
     db_search = DatabaseManager()
     return db_search.db_fetch(arg, terms, 'all')
 
-def enter_winedb(entry_input):
+def enter_db(entry_input, table='userinventory'):
     # enters a wine into the database
     # this function takes a list of the following format:
     # entry_input = [wine_id, winery, region,
@@ -48,9 +68,19 @@ def enter_winedb(entry_input):
     #                vintage, msrp, value]
     # it is critical that the list is complete, even if some
     # of the values are null
-    
+    terms = cleanup_dbinput(entry_input, table)
+
+    #arg = 'INSERT INTO ' + table + ' (wine_id, winery, region, name, varietal, wtype, vintage, msrp, value) VALUES (?,?,?,?,?,?,?,?,?)'
+    arg = 'INSERT INTO ' + table + ' ('
+    values = '('
+    for term in terms:
+        arg +=  term + ', '
+        values += ':'+ term + ', '
+    values = values.rstrip(', ') + ')'
+    arg = arg.rstrip(', ') + ') VALUES ' + values
+
     db_enter = DatabaseManager()
-    db_enter.db_execute('INSERT INTO winedata (wine_id, winery, region, name, varietal, wtype, vintage, msrp, value) VALUES (?,?,?,?,?,?,?,?,?)', entry_input)
+    db_enter.db_execute(arg, entry_input)
 
 def drop_row(wine_id, table='userinventory'):
     # drops a row from the database, if for example it is 
@@ -66,7 +96,7 @@ def drop_row(wine_id, table='userinventory'):
     db_drop = DatabaseManager()
     db_drop.db_execute(arg, (wine_id,))
 
-def update_table(update_input, table='winedata'):
+def update_table(update_input, table='userinventory'):
     # updates a specific row in a specified table
     # this function takes a list of the following format:
     # entry_input = [wine_id, winery, region,
@@ -85,3 +115,4 @@ def update_table(update_input, table='winedata'):
 # entry_input = [6, 'burnt', None,
 #                None, 'cab', 'Table',
 #                2008, None, None]
+
